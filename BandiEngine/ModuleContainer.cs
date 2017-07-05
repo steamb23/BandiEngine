c0177e666527deca448cea39fe17bba1b85b6d7d
SteamB23 <steamb23@gmail.com>
2017-07-05 16:07:52 +0900

    Remove module dependency checking

--- BandiEngine/ModuleContainer.cs
+++ BandiEngine/ModuleContainer.cs
@@ -61,21 +61,11 @@ namespace BandiEngine
 
         internal void Add(Type type, IModule module)
         {
-            // 종속성 검사
-            var requiredType = GetRequiredType(type);
-            if (requiredType.Length > 0)
-                throw new RequiredTypeException(type, requiredType);
-
             modules.Add(type, module);
         }
 
         internal bool Remove(Type type)
         {
-            // 종속성 검사
-            var dependentType = GetDependentType(type);
-            if (dependentType.Length > 0)
-                throw new DependentTypeException(type, dependentType);
-
             return modules.Remove(type);
         }
 
@@ -88,40 +78,6 @@ namespace BandiEngine
         {
             return modules[type];
         }
-
-        /// <summary>
-        /// 해당 모듈에서 요구하는 모듈의 형식을 가져옵니다. 기본적으로 이미 탑재되어있는 모듈은 제외합니다.
-        /// </summary>
-        /// <param name="type">모듈의 형식입니다.</param>
-        /// <param name="checkAllRequiredType">해당 모듈이 요구하는 모든 모듈의 형식을 가져옵니다.</param>
-        /// <returns>해당 모듈에서 요구하는 모듈의 형식입니다.</returns>
-        private Type[] GetRequiredType(Type type, bool checkAllRequiredType = false)
-        {
-            var requiredTypeAttributes = type.GetCustomAttributes(typeof(RequiredTypeAttribute), true) as RequiredTypeAttribute[];
-            var requiredTypes = new List<Type>();
-            foreach (var requiredTypeAttribute in requiredTypeAttributes)
-            {
-                if (checkAllRequiredType || !this.Contains(requiredTypeAttribute.Type))
-                    requiredTypes.Add(requiredTypeAttribute.Type);
-            }
-            return requiredTypes.ToArray();
-        }
-        /// <summary>
-        /// 해당 모듈에 의존하는 모든 모듈의 형식을 가져옵니다.
-        /// </summary>
-        /// <param name="type">모듈의 형식입니다.</param>
-        /// <returns>해당 모듈에 의존하는 모든 모듈의 형식입니다.</returns>
-        private Type[] GetDependentType(Type type)
-        {
-            var dependentType = new List<Type>();
-            foreach (var targetType in modules.Keys)
-            {
-                if (GetRequiredType(targetType, true).Contains(type))
-                    dependentType.Add(targetType);
-            }
-            return dependentType.ToArray();
-        }
-
         public IEnumerator<IModule> GetEnumerator()
         {
             return modules.Values.GetEnumerator();
