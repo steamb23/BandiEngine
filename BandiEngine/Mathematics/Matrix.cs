3d32031acdec3f29297fd1886444d463c311526e
SteamB23 <steamb23@gmail.com>
2017-07-21 11:48:05 +0900

    Added Matrix struct. (incomplete)

---
+++ BandiEngine/Mathematics/Matrix.cs
@@ -0,0 +1,162 @@
+﻿// Copyright (c) 2017 SteamB23
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandiEngine.Mathematics
+{
+    public struct Matrix
+    {
+        #region Matrix primitive type
+        public float M11;
+        public float M12;
+        public float M13;
+        public float M14;
+        public float M21;
+        public float M22;
+        public float M23;
+        public float M24;
+        public float M31;
+        public float M32;
+        public float M33;
+        public float M34;
+        public float M41;
+        public float M42;
+        public float M43;
+        public float M44;
+        #endregion
+
+        public Vector3 Up
+        {
+            get => new Vector3(M21, M22, M23);
+            set
+            {
+                M21 = value.X;
+                M22 = value.Y;
+                M23 = value.Z;
+            }
+        }
+
+        public Vector3 Down
+        {
+            get => new Vector3(-M21, -M22, -M23);
+            set
+            {
+                M21 = value.X;
+                M22 = value.Y;
+                M23 = value.Z;
+            }
+        }
+
+        public Vector3 Right
+        {
+            get => new Vector3(M11, M12, M13);
+            set
+            {
+                M11 = value.X;
+                M12 = value.Y;
+                M13 = value.Z;
+            }
+        }
+
+        public Vector3 Left
+        {
+            get => new Vector3(-M11, -M12, -M13);
+            set
+            {
+                M11 = -value.X;
+                M12 = -value.Y;
+                M13 = -value.Z;
+            }
+        }
+
+        public Vector3 Forward
+        {
+            get => new Vector3(-M31, -M32, -M33);
+            set
+            {
+                M31 = -value.X;
+                M32 = -value.Y;
+                M33 = -value.Z;
+            }
+        }
+
+        public Vector3 Backward
+        {
+            get => new Vector3(M31, M32, M33);
+            set
+            {
+                M31 = value.X;
+                M32 = value.Y;
+                M33 = value.Z;
+            }
+        }
+
+        public Matrix(float value)
+        {
+            M11 = M12 = M13 = M14 =
+            M21 = M22 = M23 = M24 =
+            M31 = M32 = M33 = M34 =
+            M41 = M42 = M43 = M44 = value;
+        }
+        public Matrix(
+            float M11, float M12, float M13, float M14,
+            float M21, float M22, float M23, float M24,
+            float M31, float M32, float M33, float M34,
+            float M41, float M42, float M43, float M44)
+        {
+            this.M11 = M11; this.M12 = M12; this.M13 = M13; this.M14 = M14;
+            this.M21 = M21; this.M22 = M22; this.M23 = M23; this.M24 = M24;
+            this.M31 = M31; this.M32 = M32; this.M33 = M33; this.M34 = M34;
+            this.M41 = M41; this.M42 = M42; this.M43 = M43; this.M44 = M44;
+        }
+
+        public Matrix(float[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+            if (values.Length < 16)
+                throw new ArgumentOutOfRangeException(nameof(values), "행렬에는 16개 이상의 입력 값이 필요합니다.");
+
+            M11 = values[0];
+            M12 = values[1];
+            M13 = values[2];
+            M14 = values[3];
+
+            M21 = values[4];
+            M22 = values[5];
+            M23 = values[6];
+            M24 = values[7];
+
+            M31 = values[8];
+            M32 = values[9];
+            M33 = values[10];
+            M34 = values[11];
+
+            M41 = values[12];
+            M42 = values[13];
+            M43 = values[14];
+            M44 = values[15];
+        }
+    }
+}
