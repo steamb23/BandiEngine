00d3a5eaccaf50e831af5df2c667717efd7c33be
SteamB23 <steamb23@gmail.com>
2017-08-07 16:49:20 +0900

    Added Audio

---
+++ BandiEngine/Audios/Channel.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2017 SteamB23
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandiEngine.Audios
+{
+    public enum Channel
+    {
+        None = 0,
+        Default = 0,
+        /// <summary>
+        /// Mono audio channel.
+        /// </summary>
+        Mono = 1,
+        /// <summary>
+        /// Stereo audio channel.
+        /// </summary>
+        Stereo = 2,
+        /// <summary>
+        /// Stereo wtih LFE audio channel.
+        /// </summary>
+        StereoLFE = 3,
+        /// <summary>
+        /// Front Rear Stereo audio channel.
+        /// </summary>
+        Quadraphonic = 4,
+        /// <summary>
+        /// 5.0 Surround Channel.
+        /// </summary>
+        Surround5_0 = 5,
+        /// <summary>
+        /// 5.1 Surround Channel.
+        /// </summary>
+        Surround5_1 = 6,
+        /// <summary>
+        /// 6.1 Surround Channel.
+        /// </summary>
+        Surround6_1 = 7,
+        /// <summary>
+        /// 7.1 Surround Channel.
+        /// </summary>
+        Surround7_1 = 8
+    }
+}
---
+++ BandiEngine/Audios/DirectX/AudioEngine.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) 2017 SteamB23
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using SharpDX;
+using XAudio2 = SharpDX.XAudio2;
+
+namespace BandiEngine.Audios.DirectX
+{
+    public class AudioEngine : Audios.AudioEngine
+    {
+        XAudio2.XAudio2 xAudio2Device;
+        XAudio2.MasteringVoice masterVoice;
+
+        public AudioEngine(Channel audioChannel = Channel.Default)
+        {
+            CreateDevice();
+            CreateMasterVoice(audioChannel);
+        }
+
+        void CreateDevice()
+        {
+            Utilities.Dispose(ref xAudio2Device);
+
+            XAudio2.XAudio2Flags flags = RuntimeSymbol.DEBUG ? XAudio2.XAudio2Flags.DebugEngine : XAudio2.XAudio2Flags.None;
+            xAudio2Device = new XAudio2.XAudio2(flags, XAudio2.ProcessorSpecifier.DefaultProcessor);
+        }
+
+        void CreateMasterVoice(Channel audioChannel)
+        {
+            Utilities.Dispose(ref xAudio2Device);
+
+            masterVoice = new XAudio2.MasteringVoice(xAudio2Device, (int)audioChannel, XAudio2.XAudio2.DefaultSampleRate);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            Utilities.Dispose(ref xAudio2Device);
+            Utilities.Dispose(ref xAudio2Device);
+        }
+    }
+}
